807e3694ed0fc2d8527e8d5c2e4fdaafd0e1f2d9
Taylor <trippy@alfains.com>
2025-12-16 08:40:42 -0600

    First Try

---
+++ Programs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static int Mod(int a, int m) => ((a % m) + m) % m;
+
+    static void Main()
+    {
+        int pos = 50;
+        int hitsZero = 0;
+
+        foreach (var raw in File.ReadLines("input.txt"))
+        {
+            var line = raw.Trim();
+            if (line.Length == 0) continue;
+
+            char dir = line[0];
+            int dist = int.Parse(line.Substring(1));
+
+            if (dir == 'L') pos = Mod(pos - dist, 100);
+            else if (dir == 'R') pos = Mod(pos + dist, 100);
+            else throw new Exception($"Bad rotation: {line}");
+
+            if (pos == 0) hitsZero++;
+        }
+
+        Console.WriteLine(hitsZero);
+    }
+}
